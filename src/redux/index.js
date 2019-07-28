21eb0a86c810427ddd54de552b42911202e0da92
ainice7 <makaroff.seven@gmail.com>
2019-07-28 14:28:36 +0300

    Create index.js

---
+++ src/redux/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+import { movies } from './movies';
+import { loading } from './loading';
+
+export const rootReducer = combineReducers({
+    loading: loading,
+    data: movies
+})
