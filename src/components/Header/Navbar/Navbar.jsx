b3791694eeefb4857fe084865d4a4c65d0abb0f6
ainice7 <makaroff.seven@gmail.com>
2019-07-28 14:28:08 +0300

    Create Navbar.jsx

---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './NavbarContainer.css';
+
+const Navbar = () => {
+
+    return <div className='navbar-container' >
+        <nav role="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <Link to='/sessions' ><li>Sessions</li></Link>
+                    
+                </ul>
+            </div>
+        </nav>
+        <div>
+            <p>STAR WARS</p>
+            <p>Cinema</p>
+        </div>
+    </div>
+}
+
+export default Navbar;
